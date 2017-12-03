b5604f751a24ce58411aabe2267d5fa754d2d844
Saatvik Arya <aryasaatvik@gmail.com>
2017-12-03 14:09:39 -0600

    removed nibs and added storyboards

--- SocialTodo/ViewControllers/ViewController.swift
+++ SocialTodo/ViewControllers/ViewController.swift
@@ -15,45 +15,49 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    let addFriends: AddFriendsViewController = AddFriendsViewController(nibName: "AddFriendsViewController", bundle: nil)
-    let friends: FriendsViewController = FriendsViewController(nibName: "FriendsViewController", bundle: nil)
-    let todoLists: TodoListsViewController = TodoListsViewController(nibName: "TodoListsViewController", bundle: nil)
-    let profile: ProfileViewController = ProfileViewController(nibName: "ProfileViewController", bundle: nil)
+		let addFriendsStoryboard = UIStoryboard(name: "AddFriends", bundle: nil)
+		let addFriends = addFriendsStoryboard.instantiateViewController(withIdentifier: "AddFriendsNav")
+		self.addChildViewController(addFriends)
+		self.scrollView.addSubview(addFriends.view)
+		addFriends.didMove(toParentViewController: self)
 
-    self.addChildViewController(addFriends)
-    self.scrollView.addSubview(addFriends.view)
-    addFriends.didMove(toParentViewController: self)
+		var addFriendsFrame: CGRect = addFriends.view.frame
+		addFriendsFrame.origin.x = 0 * self.view.frame.width
+		addFriends.view.frame = addFriendsFrame
 
-    self.addChildViewController(friends)
-    self.scrollView.addSubview(friends.view)
-    friends.didMove(toParentViewController: self)
+		let friendsStoryboard = UIStoryboard(name: "Friends", bundle: nil)
+		let friends = friendsStoryboard.instantiateViewController(withIdentifier: "FriendsNav")
+		self.addChildViewController(friends)
+		self.scrollView.addSubview(friends.view)
+		friends.didMove(toParentViewController: self)
 
-    self.addChildViewController(todoLists)
-    self.scrollView.addSubview(todoLists.view)
-    todoLists.didMove(toParentViewController: self)
+		var friendsFrame: CGRect = friends.view.frame
+		friendsFrame.origin.x = self.view.frame.width
+		friends.view.frame = friendsFrame
 
-    self.addChildViewController(profile)
-    self.scrollView.addSubview(profile.view)
-    profile.didMove(toParentViewController: self)
+		let todoListsStoryboard = UIStoryboard(name: "TodoLists", bundle: nil)
+		let todoLists = todoListsStoryboard.instantiateViewController(withIdentifier: "TodoListsNav")
+		self.addChildViewController(todoLists)
+		self.scrollView.addSubview(todoLists.view)
+		todoLists.didMove(toParentViewController: self)
 
-    var addFriendsFrame: CGRect = addFriends.view.frame
-    addFriendsFrame.origin.x = 0 * self.view.frame.width
-    addFriends.view.frame = addFriendsFrame
+		var todoListsFrame: CGRect = todoLists.view.frame
+		todoListsFrame.origin.x = 2 * self.view.frame.width
+		todoLists.view.frame = todoListsFrame
 
-    var friendsFrame: CGRect = friends.view.frame
-    friendsFrame.origin.x = self.view.frame.width
-    friends.view.frame = friendsFrame
+		self.scrollView.setContentOffset(CGPoint(x: todoListsFrame.origin.x, y: todoListsFrame.origin.y), animated: true)
 
-    var todoListsFrame: CGRect = todoLists.view.frame
-    todoListsFrame.origin.x = 2 * self.view.frame.width
-    todoLists.view.frame = todoListsFrame
+		let profileStoryboard = UIStoryboard(name: "Profile", bundle: nil)
+		let profile = profileStoryboard.instantiateViewController(withIdentifier: "ProfileNav")
+		self.addChildViewController(profile)
+		self.scrollView.addSubview(profile.view)
+		profile.didMove(toParentViewController: self)
 
-    var profileFrame: CGRect = profile.view.frame
-    profileFrame.origin.x = 3 * self.view.frame.width
-    profile.view.frame = profileFrame
+		var profileFrame: CGRect = profile.view.frame
+		profileFrame.origin.x = 3 * self.view.frame.width
+		profile.view.frame = profileFrame
 
     self.scrollView.contentSize = CGSize(width: self.view.frame.width * 4, height: self.view.frame.size.height)
-    self.scrollView.setContentOffset(CGPoint(x: todoListsFrame.origin.x, y: todoListsFrame.origin.y), animated: true)
   }
 
   override func didReceiveMemoryWarning() {
