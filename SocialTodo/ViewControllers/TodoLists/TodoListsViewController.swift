dc2fd317f44567dd3f74bea84ed28a87fa07223d
Saatvik Arya <aryasaatvik@gmail.com>
2017-12-03 20:24:30 -0600

    TodoList Table View Design

--- SocialTodo/ViewControllers/TodoLists/TodoListsViewController.swift
+++ SocialTodo/ViewControllers/TodoLists/TodoListsViewController.swift
@@ -8,27 +8,43 @@
 
 import UIKit
 
-class TodoListsViewController: UIViewController {
+class TodoListsViewController: UIViewController, UITableViewDataSource {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	@IBOutlet weak var tableView: UITableView!
 
-        // Do any additional setup after loading the view.
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+		tableView.dataSource = self
+	}
 
-    /*
-    // MARK: - Navigation
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 5
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "TL-Cell", for: indexPath)
+		return cell
+	}
+
+	/*
+	// MARK: - Navigation
+	
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+	// Get the new view controller using segue.destinationViewController.
+	// Pass the selected object to the new view controller.
+	}
+	*/
 
 }
